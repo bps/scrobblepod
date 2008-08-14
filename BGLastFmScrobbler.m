10204cbaff82d34e2e90b70d0b2736388b709d47
Ben Gummer <bengummer@gmail.com>
2008-08-14 21:04:44 +1000

    Re-enabled scrobbling HTTP error codes

--- BGLastFmScrobbler.m
+++ BGLastFmScrobbler.m
@@ -88,8 +88,8 @@
 		NSHTTPURLResponse *response = nil;
 		NSData *scrobbleResponseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&postingError];
 		
-//		NSLog(@"STATUS CODE: %d",[response statusCode]);		
-//		NSLog(@" ERROR CODE: %d",[postingError code]);
+		NSLog(@"STATUS CODE: %d",[response statusCode]);		
+		NSLog(@" ERROR CODE: %d",[postingError code]);
 		
 		if (scrobbleResponseData!=nil && [postingError code]!=-1001 && [response statusCode]==200) {
 			NSString *scrobbleResponseString = [[NSString alloc] initWithData:scrobbleResponseData encoding:NSUTF8StringEncoding];
